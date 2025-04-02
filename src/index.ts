bc240ca2772212f004c23c0b88dd2a0c50ea6ba1
Ramon Wirsch <ramon.wirsch@gmail.com>
2025-04-02 16:32:46 +0200

    Also exporting file-format-spec (for later building speedscope format directly) and importing trace-event format (for now)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from './import/linux-tools-perf';
+export * from './import/trace-event';
 export * from './import/utils';
+export * from './lib/file-format-spec';
 export * from './lib/file-format';
